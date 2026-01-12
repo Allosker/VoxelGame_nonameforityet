19d545b63971c662cb747a505aa5caeb4927866f
Allosker <allosker.pro@gmail.com>
2026-01-12 19:52:21 +0100

    did sope stuff

--- files/src/main.cpp
+++ files/src/main.cpp
@@ -30,93 +30,89 @@ float deltaTime{};
 
 struct Vertex
 {
-	vec3f pos{};
-	vec3f color{};
-	vec3f normal{};
+
+	Vertex(const vec3f& vec1, const vec3f& vec2, const vec3f& vec3 = {})
+		: m_data{ vec1, vec2, vec3 }
+	{
+	}
+
+	union
+	{
+		vec3f pos;
+		vec3f color;
+		vec3f normal;
+
+		std::array<vec3f, 3> m_data;
+	};
+
+	const float* data() const noexcept
+	{
+		return m_data[0].data_ptr();
+	}
+
+	float* data() noexcept
+	{
+		return m_data[0].data_ptr();
+	}
 };
 
 struct Faces
 {
-	/*Faces*/
-	std::array<Vertex, 4> front
-	{
-		/*Front*/
-		Vertex
-		{{-0.5, 0, 0},  /*Left-Down*/  {0.1, 0.1, 0.1}},
-		{{-0.5, 1, 0},  /*Left-Up*/    {0.1, 0.1, 0.1}},
-		{{ 0.5, 0, 0},  /*Right-Down*/ {0.1, 0.1, 0.1}},
-		{{ 0.5, 1, 0},  /*Right-Up*/   {0.1, 0.1, 0.1}}
-	};
+		std::array<Vertex, 24> m_data
+		{
+			/*Front*/
+			Vertex
+			{{-0.5, 0, 0},  /*Left-Down*/  {0.1, 0.1, 0.1}},
+			{{-0.5, 1, 0},  /*Left-Up*/    {0.1, 0.1, 0.1}},
+			{{ 0.5, 0, 0},  /*Right-Down*/ {0.1, 0.1, 0.1}},
+			{{ 0.5, 1, 0},  /*Right-Up*/   {0.1, 0.1, 0.1}},
 
-	std::array<Vertex, 4> back
-	{
-		/*Back*/
-		Vertex
-		{{ 0.5, 0, -1},  /*Left-Down*/  {0.2, 0.2, 0.2}},
-		{{ 0.5, 1, -1},  /*Left-Up*/    {0.2, 0.2, 0.2}},
-		{{-0.5, 0, -1},  /*Right-Down*/ {0.2, 0.2, 0.2}},
-		{{-0.5, 1, -1},  /*Right-Up*/   {0.2, 0.2, 0.2}},
-	};
+			/*Back*/
+			Vertex
+			{{ 0.5, 0, -1},  /*Left-Down*/  {0.2, 0.2, 0.2}},
+			{{ 0.5, 1, -1},  /*Left-Up*/    {0.2, 0.2, 0.2}},
+			{{-0.5, 0, -1},  /*Right-Down*/ {0.2, 0.2, 0.2}},
+			{{-0.5, 1, -1},  /*Right-Up*/   {0.2, 0.2, 0.2}},
 
-	std::array<Vertex, 4> up
-	{
-		/*Up*/
-		Vertex
-		{{ 0.5, 1, 0},   /*Left-Down*/  {0.3, 0.3, 0.3}},
-		{{-0.5, 1, 0},   /*Left-Up*/    {0.3, 0.3, 0.3}},
-		{{ 0.5, 1, -1},  /*Right-Down*/ {0.3, 0.3, 0.3}},
-		{{-0.5, 1, -1},  /*Right-Up*/   {0.3, 0.3, 0.3}},
-	};
+			/*Up*/
+			Vertex
+			{{ 0.5, 1, 0},   /*Left-Down*/  {0.3, 0.3, 0.3}},
+			{{-0.5, 1, 0},   /*Left-Up*/    {0.3, 0.3, 0.3}},
+			{{ 0.5, 1, -1},  /*Right-Down*/ {0.3, 0.3, 0.3}},
+			{{-0.5, 1, -1},  /*Right-Up*/   {0.3, 0.3, 0.3}},
 
-	std::array<Vertex, 4> down
-	{
-		/*Down*/
-		Vertex
-		{{ 0.5, 0, -1},  /*Right-Down*/ {0.4, 0.4, 0.4}},
-		{{-0.5, 0, -1},  /*Right-Up*/   {0.4, 0.4, 0.4}},
-		{{ 0.5, 0, 0},   /*Left-Down*/  {0.4, 0.4, 0.4}},
-		{{-0.5, 0, 0},   /*Left-Up*/    {0.4, 0.4, 0.4}},
-	};
+			/*Down*/
+			Vertex
+			{{ 0.5, 0, -1},  /*Right-Down*/ {0.4, 0.4, 0.4}},
+			{{-0.5, 0, -1},  /*Right-Up*/   {0.4, 0.4, 0.4}},
+			{{ 0.5, 0, 0},   /*Left-Down*/  {0.4, 0.4, 0.4}},
+			{{-0.5, 0, 0},   /*Left-Up*/    {0.4, 0.4, 0.4}},
 
-	std::array<Vertex, 4> left
-	{
-		/*Left*/
-		Vertex
-		{{ 0.5, 0, 0},   /*Right-Down*/ {0.5, 0.5, 0.5}},
-		{{ 0.5, 1, 0},   /*Right-Up*/   {0.5, 0.5, 0.5}},
-		{{ 0.5, 0, -1},  /*Left-Down*/  {0.5, 0.5, 0.5}},
-		{{ 0.5, 1, -1},  /*Left-Up*/    {0.5, 0.5, 0.5}},
-	};
+			/*Left*/
+			Vertex
+			{{ 0.5, 0, 0},   /*Right-Down*/ {0.5, 0.5, 0.5}},
+			{{ 0.5, 1, 0},   /*Right-Up*/   {0.5, 0.5, 0.5}},
+			{{ 0.5, 0, -1},  /*Left-Down*/  {0.5, 0.5, 0.5}},
+			{{ 0.5, 1, -1},  /*Left-Up*/    {0.5, 0.5, 0.5}},
 
-	std::array<Vertex, 4> right
-	{
-		/*Right*/
-		Vertex
-		{{-0.5, 0, -1},  /*Right-Down*/ {0.6, 0.6, 0.6}},
-		{{-0.5, 1, -1},  /*Right-Up*/   {0.6, 0.6, 0.6}},
-		{{-0.5, 0, 0},   /*Left-Down*/  {0.6, 0.6, 0.6}},
-		{{-0.5, 1, 0},   /*Left-Up*/    {0.6, 0.6, 0.6}},
-	};
+			/*Right*/
+			Vertex
+			{{-0.5, 0, -1},  /*Right-Down*/ {0.6, 0.6, 0.6}},
+			{{-0.5, 1, -1},  /*Right-Up*/   {0.6, 0.6, 0.6}},
+			{{-0.5, 0, 0},   /*Left-Down*/  {0.6, 0.6, 0.6}},
+			{{-0.5, 1, 0},   /*Left-Up*/    {0.6, 0.6, 0.6}},
+		};
 
-	const float* begin() const noexcept
+	const float* data() const noexcept
 	{
-		return &front[0].pos[0];
+		return m_data[0].data();
 	}
 
-	float* begin() noexcept
+	float* data() noexcept
 	{
-		return &front[0].pos[0];
+		return m_data[0].data();
 	}
 
-	const float* end() const noexcept
-	{
-		return &right[3].normal[3];
-	}
-
-	float* end() noexcept
-	{
-		return &right[3].normal[3];
-	}
 };
 
 struct Cube
@@ -138,20 +134,177 @@ class Chunk
 {
 public:
 
-
-
-	const Vertex* cubeData() const noexcept
+	Chunk(std::vector<std::array<Vertex, 4>> gfaces, GLuint gebo, const vec3f& pos={})
 	{
-		return m_meshes.data();
+		m_pos = pos;
+
+		// Create VAO for chunk
+		glGenVertexArrays(1, &m_vao_id);
+		glBindVertexArray(m_vao_id);
+
+		// Create VBO for cube translations
+		glGenBuffers(1, &m_vbo_id);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_id);
+
+		fill();
+		
+
+		// Bind Global iVBO -> Faces for each Cube
+		GLuint vbo_faces{};
+
+		glGenBuffers(1, &vbo_faces);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_faces);
+
+		std::int32_t dimensions{ 32 };
+		std::int32_t dMax{ dimensions * dimensions * dimensions };
+
+
+		for (std::int64_t x{}; x < dimensions; x++)
+		{
+			for (std::int64_t y{}; y < dimensions; y++)
+			{
+				for (std::int64_t z{}; z < dimensions; z++)
+				{
+
+					typedef Cube::Type t;
+
+					std::array<t, 6> faces{};
+
+
+					if (z + 1 < 32)
+						faces[0] = m_cubes[((z + 1) * dimensions * dimensions) + (y * dimensions) + x];
+					else
+						faces[0] = t::Empty;
+
+					if(z - 1 >= 0)
+						faces[1] = m_cubes[((z - 1) * dimensions * dimensions) + (y * dimensions) + x];
+					else
+						faces[1] = t::Empty;
+
+
+					if (y + 1 < 32)
+						faces[2] = m_cubes[(z * dimensions * dimensions) + ((y + 1) *dimensions) + x];
+					else
+						faces[2] = t::Empty;
+
+					if (y - 1 >= 0)
+						faces[3] = m_cubes[(z * dimensions * dimensions) + ((y - 1) * dimensions) + x];
+					else
+						faces[3] = t::Empty;
+
+
+					if (x + 1 < 32)
+						faces[4] = m_cubes[(z * dimensions * dimensions) + (y * dimensions) + x + 1];
+					else
+						faces[4] = t::Empty;
+
+					if (x - 1 >= 0)
+						faces[5] = m_cubes[(z * dimensions * dimensions) + (y * dimensions) + x - 1];
+					else
+						faces[5] = t::Empty;
+					
+
+					
+					if (faces[0] == t::Empty)
+					{
+						m_trans.push_back({ x + m_pos.x, y + m_pos.y, z + m_pos.z });
+						m_meshes.push_back(gfaces[0]);
+					}
+
+					if (faces[1] == t::Empty)
+					{
+						m_trans.push_back({ x + m_pos.x, y + m_pos.y, z + m_pos.z + 1 });
+						m_meshes.push_back(gfaces[1]);
+					}
+
+					if (faces[2] == t::Empty)
+					{
+						m_trans.push_back({ x + m_pos.x, y + m_pos.y, z + m_pos.z });
+						m_meshes.push_back(gfaces[2]);
+					}
+
+					if (faces[3] == t::Empty)
+					{
+						m_trans.push_back({ x + m_pos.x, y + m_pos.y + 1, z + m_pos.z });
+						m_meshes.push_back(gfaces[3]);
+					}
+
+					if (faces[4] == t::Empty)
+					{
+						m_trans.push_back({ x + m_pos.x, y + m_pos.y, z + m_pos.z });
+						m_meshes.push_back(gfaces[4]);
+					}
+
+					if (faces[5] == t::Empty)
+					{
+						m_trans.push_back({ x + m_pos.x + 1, y + m_pos.y, z + m_pos.z });
+						m_meshes.push_back(gfaces[5]);
+					}
+				}
+			}
+
+		}
+
+		for (size_t i{}; i < m_meshes.size(); i++)
+		{
+			m_meshes[i][0].pos += m_trans[i];
+			m_meshes[i][1].pos += m_trans[i];
+			m_meshes[i][2].pos += m_trans[i];
+			m_meshes[i][3].pos += m_trans[i];
+		}
+
+		//glBufferData(GL_ARRAY_BUFFER, m_trans.size() * sizeof(vec3f), m_trans.data(), GL_STATIC_DRAW);
+
+		//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vec3f), nullptr);
+		//glEnableVertexAttribArray(2);
+		//glVertexAttribDivisor(2, 1);
+
+		glBufferData(GL_ARRAY_BUFFER, m_meshes.size() * (sizeof(Vertex) * 4), m_meshes.data(), GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(sizeof(vec3f)));
+		glEnableVertexAttribArray(1);
+
+		// Bind Global gEBO
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gebo);
+	}
+
+	void bind()
+	{
+		glBindVertexArray(m_vao_id);
+	}
+
+	size_t getNumberTranslations()
+	{
+		return m_trans.size();
+	}
+
+	void fill()
+	{
+		for (int i{}; i < 32; i++)
+		{
+			for (int j{}; j < 32; j++)
+			{
+				for (int k{}; k < 32; k++)
+				{
+					m_cubes.push_back(Cube::Type::Empty);
+				}
+			}
+		}
 	}
 
 private:
 
-	std::array<Cube, 32'768> m_cube_data{};
-	std::vector<Vertex> m_meshes{};
+	std::vector<vec3f> m_trans{};
+	std::vector<Cube::Type> m_cubes{};
+	std::vector<std::array<Vertex, 4>> m_meshes{};
 
-	std::uint32_t m_vbo_id{};
+	GLuint m_vbo_id{};
+	GLuint m_vao_id{};
 
+	vec3f m_pos{};
 };
 
 int main()
@@ -173,9 +326,7 @@ int main()
 
 	glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	Faces faces{};
 
-	Cube cube{};
 
 	std::vector<std::uint32_t> indices
 	{
@@ -206,64 +357,35 @@ int main()
 	};
 
 
-	std::uint32_t vbo{};
+	
 
-	glGenBuffers(1, &vbo);
+	Faces faces{};
+	std::vector<std::array<Vertex, 4>> cube_faces{};
 
-	std::uint32_t vao{};
+	for (size_t i{}; i < faces.m_data.size(); i += 4)
+	{
+		cube_faces.push_back({ faces.m_data[i], faces.m_data[i + 1], faces.m_data[i + 2], faces.m_data[i + 3] });
+	}
 
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube), faces.begin(), GL_STATIC_DRAW);
+	std::uint32_t gebo{};
 
-	std::uint32_t ebo{};
+	glGenBuffers(1, &gebo);
 
-	glGenBuffers(1, &ebo);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gebo);
 
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(*indices.data()), indices.data(), GL_STATIC_DRAW);
 
 
+	Chunk chunk{ cube_faces, gebo, {0, 0, 0} };
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(sizeof(vec3f)));
-	glEnableVertexAttribArray(1);
+	Chunk chunk2{ cube_faces, gebo, {0, 0, 40} };
 
 
 	render::Shader shader{ SHADER_PATH"shader.vert", SHADER_PATH"shader.frag" };
 
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-
-
-	std::vector<vec3f> trans{};
-
-	for (int i{}; i < 100; i++)
-	{
-		for (int j{}; j < 100; j++)
-		{
-			for (int k{}; k < 100; k++)
-				trans.push_back({ (float)i, (float)k, (float)j});
-		}
-	}
-
-	std::uint32_t ivbo{};
-	glGenBuffers(1, &ivbo);
-	glBindBuffer(GL_ARRAY_BUFFER, ivbo); 
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3f) * trans.size(), trans.data(), GL_STATIC_DRAW);
-
-
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vec3f), nullptr);
-	glEnableVertexAttribArray(2);
-	glVertexAttribDivisor(2, 1);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glEnable(GL_CULL_FACE);
 
 
 	float lastFrame{};
@@ -281,7 +403,7 @@ int main()
 
 		mat4f model{ mpml::Identity4<float> };
 		mat4f view{ mpml::lookAt(camera.pos, camera.front_dir + camera.pos, camera.up_dir) };
-		mat4f proj{ mpml::perspective(mpml::Angle::fromDegrees(45), window.getSize().x, window.getSize().y, 0.1f, 100.f) };
+		mat4f proj{ mpml::perspective(mpml::Angle::fromDegrees(45), window.getSize().x, window.getSize().y, 0.1f, 100.f, true) };
 
 		//model = mpml::rotate(model, mpml::Angle::fromDegrees(glfwGetTime() * 10), { 0, 0.5, 0 });
 
@@ -292,9 +414,12 @@ int main()
 		shader.setValue("proj", proj);
 
 		
-		
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glDrawElementsInstanced(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr, trans.size());
+
+			chunk.bind();
+			glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+
+			/*chunk2.bind();
+			glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);*/
 
 		window.clearEvents();
 		window.display();
@@ -350,7 +475,7 @@ void inputs(const Wai::Window& window) noexcept
 	using namespace Wai;
 	using b = Buttons;
 
-	float camSpeed{ 5.f * deltaTime };
+	float camSpeed{ 25.f * deltaTime };
 
 	if (window.keyPressed(b::Escape))
 		window.close();
